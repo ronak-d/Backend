9e4004dd57bf5a53e577889fd800841b6f714ac2
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-29 15:50:59 +0530

    Es6 not working

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -4,24 +4,32 @@ const app = express();
 //  Express always look for Methods + Route
 // eg => get + /users
 
-app.use(middlewares)    // USE is for middlewares as this comes in action execution of code goes to 
-                        // line no.17 via ignoring all the code written before middlewares function.
+app.use(middlewares1)    // USE is for middlewares as this comes in action execution of code goes to 
+app.use(middlewares2)    // line no.17 via ignoring all the code written before middlewares function.
 
 app.get("/users",(req,res) => {
-    console.log("then method and routes are working");
+    console.log("Iam working after middlewares and arrow function will not be executed");
     return res.send("hello ronapp")
 })
 
 // Middlewares are like brokers we can say they can alter the REQUEST or RESPONSE.
 // accepts 3 props - REQ,RES,NEXT.
 
-function middlewares(req, res, next) {
+function middlewares1(req, res, next) {
     console.log("First middleware are running");
     next();     // this next() will give execution of code just after whrere middlewares() app.USE called.
-                // line no.9 to 12.
+    console.log('middleOne over')   // line no.9 to 12.
+}
+
+function middlewares2(req, res, next) {
+    console.log("Second middleware are running");
+    next();     // this next() will give execution of code just after whrere middlewares() app.USE called.
+    console.log('middleTwo over');
 }
 
 
+
+// to listen the server on the browser
 app.listen(3000, ()=>{
-    console.log("listening on port")
-})
+    console.log("listening on port 3000")
+});

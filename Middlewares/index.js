0bf74a490f6ea98e654db0329f6660026076e70f
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-29 13:10:31 +0530

    Middlewares learned

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(middlewares)    // USE is for middlewares as this comes in action execution of code goes to 
                         // line no.17 via ignoring all the code written before middlewares function.
 
-app.get("ronapp",(req,res) => {
+app.get("/users",(req,res) => {
+    console.log("then method and routes are working");
     return res.send("hello ronapp")
 })
 
@@ -15,12 +16,12 @@ app.get("ronapp",(req,res) => {
 // accepts 3 props - REQ,RES,NEXT.
 
 function middlewares(req, res, next) {
-    console.log("middleware are running");
+    console.log("First middleware are running");
     next();     // this next() will give execution of code just after whrere middlewares() app.USE called.
                 // line no.9 to 12.
 }
 
 
-app.listen(8080, ()=>{
-    console.log("listening on port 8080")
+app.listen(3000, ()=>{
+    console.log("listening on port")
 })
